0b799335179a35c948ced4f77ff6df8897c36c02
abhik7872 <chatterjeeabhik90@gmail.com>
2022-09-14 00:06:50 +0530

    Navbar Done

--- pwdGen/views.py
+++ pwdGen/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+data = [
+    {
+        'for': 'facebook',
+        'password': '13@fdSgg89W',
+    },
+    {
+        'for': 'netflix',
+        'password': '434@%#hjgAfgh',
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'pwd/home.html')
+    context = {
+        'posts': data,
+    }
+    return render(request, 'pwd/home.html', context)
